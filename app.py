b1c669d13b9da62b60f4ca2f439a37dc6e4949d8
AnkitAnand28 <87243712+AnkitAnand28@users.noreply.github.com>
2024-10-17 21:33:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-!pip install --upgrade tensorflow
 import numpy as np 
 import tensorflow as tf 
 from sklearn.preprocessing import StandardScaler, LabelEncoder, OneHotEncoder
